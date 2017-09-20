733daafac07efab137acbfe9af370291f4ac2f38
Ruixuel <liaoruixue312@gmail.com>
2017-09-20 17:23:17 -0400

    Add P2P logic in HelloWorldClient and HelloWorldServer

--- examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -1,3 +1,14 @@
+
+package io.grpc.examples.helloworld;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.*;
+
 /*
  * Copyright 2015, gRPC Authors All rights reserved.
  *
@@ -14,72 +25,89 @@
  * limitations under the License.
  */
 
-package io.grpc.examples.helloworld;
-
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.StatusRuntimeException;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  * A simple client that requests a greeting from the {@link HelloWorldServer}.
  */
 public class HelloWorldClient {
-  private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+	private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
 
-  private final ManagedChannel channel;
-  private final GreeterGrpc.GreeterBlockingStub blockingStub;
+	private final ManagedChannel channel;
+	private final GreeterGrpc.GreeterBlockingStub blockingStub;
+	private String host;
 
-  /** Construct client connecting to HelloWorld server at {@code host:port}. */
-  public HelloWorldClient(String host, int port) {
-    this(ManagedChannelBuilder.forAddress(host, port)
-        // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
-        // needing certificates.
-        .usePlaintext(true)
-        .build());
-  }
+	/**
+	 * Construct client connecting to HelloWorld server at {@code host:port}.
+	 */
+	public HelloWorldClient(String host, int port) {
+		this(ManagedChannelBuilder.forAddress(host, port)
+				// Channels are secure by default (via SSL/TLS). For the example
+				// we disable TLS to avoid
+				// needing certificates.
+				.usePlaintext(true).build());
+		this.host = host;
+	}
 
-  /** Construct client for accessing RouteGuide server using the existing channel. */
-  HelloWorldClient(ManagedChannel channel) {
-    this.channel = channel;
-    blockingStub = GreeterGrpc.newBlockingStub(channel);
-  }
+	public String getHost() {
+		return host;
+	}
 
-  public void shutdown() throws InterruptedException {
-    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-  }
+	/**
+	 * Construct client for accessing RouteGuide server using the existing
+	 * channel.
+	 */
+	HelloWorldClient(ManagedChannel channel) {
+		this.channel = channel;
+		blockingStub = GreeterGrpc.newBlockingStub(channel);
+	}
 
-  /** Say hello to server. */
-  public void greet(String name) {
-    logger.info("Will try to greet " + name + " ...");
-    HelloRequest request = HelloRequest.newBuilder().setName(name).build();
-    HelloReply response;
-    try {
-      response = blockingStub.sayHello(request);
-    } catch (StatusRuntimeException e) {
-      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-      return;
-    }
-    logger.info("Greeting: " + response.getMessage());
-  }
+	public void shutdown() throws InterruptedException {
+		channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+	}
 
-  /**
-   * Greet server. If provided, the first element of {@code args} is the name to use in the
-   * greeting.
-   */
-  public static void main(String[] args) throws Exception {
-    HelloWorldClient client = new HelloWorldClient("localhost", 50051);
-    try {
-      /* Access a service running on the local machine on port 50051 */
-      String user = "world";
-      if (args.length > 0) {
-        user = args[0]; /* Use the arg as the name to greet if provided */
-      }
-      client.greet(user);
-    } finally {
-      client.shutdown();
-    }
-  }
+	public void greet(String message, String userName) throws StatusRuntimeException {
+		logger.info("Will try to send message: " + '"' + message + '"' + " to " + userName);
+		HelloRequest request = HelloRequest.newBuilder().setName(message).build();
+		HelloReply response = null;
+		response = blockingStub.sayHello(request);
+		if (response != null) {
+			logger.info("Greeting: " + response.getMessage());
+		}
+	}
+
+	/**
+	 * Greet server. If provided, the first element of {@code args} is the name
+	 * to use in the greeting.
+	 */
+	public static void main(String[] args) {
+		String IP = "localhost"; // Can be local IP
+		Map<String, String> IPMap = new HashMap<String, String>();
+		IPMap.put("localhost", "Me");
+		IPMap.put("128.237.129.30", "User1");
+		IPMap.put("128.237.130.169", "User2");
+
+		HelloWorldClient[] clients = new HelloWorldClient[1];
+
+		clients[0] = new HelloWorldClient(IP, 50051);
+		// clients[1] = new HelloWorldClient(IP1, 50051);
+		// clients[2] = new HelloWorldClient(IP2, 50051);
+
+		/* Access a service running on the local machine on port 50051 */
+		String user = "world";
+		if (args.length > 0) {
+			user = args[0]; /* Use the arg as the name to greet if provided */
+		}
+		for (int i = 0; i < 1; i++) {
+			try {
+				clients[i].greet(user, IPMap.get(clients[i].getHost()));
+			} catch (Exception e) {
+				System.out.println(IPMap.get(clients[i].getHost()) + " is offline.");
+			} finally {
+				try {
+					clients[i].shutdown();
+				} catch (Exception ex) {
+					System.out.println("Error: shutdown " + clients[i].getHost());
+				}
+			}
+		}
+	}
 }
--- examples/src/main/java/io/grpc/examples/helloworld/HelloWorldServer.java
+++ examples/src/main/java/io/grpc/examples/helloworld/HelloWorldServer.java
@@ -21,65 +21,104 @@ import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.util.logging.Logger;
+import java.util.*;
 
 /**
  * Server that manages startup/shutdown of a {@code Greeter} server.
  */
 public class HelloWorldServer {
-  private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
+	private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
 
-  private Server server;
+	private Server server;
+	private static Map<String, String> IPMap = new HashMap<String, String>();
+	/* The port on which the server should run */
+	private final static int port = 50051;
 
-  private void start() throws IOException {
-    /* The port on which the server should run */
-    int port = 50051;
-    server = ServerBuilder.forPort(port)
-        .addService(new GreeterImpl())
-        .build()
-        .start();
-    logger.info("Server started, listening on " + port);
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      @Override
-      public void run() {
-        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-        System.err.println("*** shutting down gRPC server since JVM is shutting down");
-        HelloWorldServer.this.stop();
-        System.err.println("*** server shut down");
-      }
-    });
-  }
+	private void start() throws IOException {
+		server = ServerBuilder.forPort(port).addService(new GreeterImpl()).build().start();
+		logger.info("Server started, listening on " + port);
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				// Use stderr here since the logger may have been reset by its
+				// JVM shutdown hook.
+				System.err.println("*** shutting down gRPC server since JVM is shutting down");
+				HelloWorldServer.this.stop();
+				System.err.println("*** server shut down");
+			}
+		});
+	}
 
-  private void stop() {
-    if (server != null) {
-      server.shutdown();
-    }
-  }
+	private void stop() {
+		if (server != null) {
+			server.shutdown();
+		}
+	}
 
-  /**
-   * Await termination on the main thread since the grpc library uses daemon threads.
-   */
-  private void blockUntilShutdown() throws InterruptedException {
-    if (server != null) {
-      server.awaitTermination();
-    }
-  }
+	/**
+	 * Await termination on the main thread since the grpc library uses daemon
+	 * threads.
+	 */
+	private void blockUntilShutdown() throws InterruptedException {
+		if (server != null) {
+			server.awaitTermination();
+		}
+	}
 
-  /**
-   * Main launches the server from the command line.
-   */
-  public static void main(String[] args) throws IOException, InterruptedException {
-    final HelloWorldServer server = new HelloWorldServer();
-    server.start();
-    server.blockUntilShutdown();
-  }
+	/**
+	 * Initialize IP List
+	 */
+	private void initalizeIPList() throws InterruptedException {
+		IPMap.put("128.237.130.169", "User1");
+		IPMap.put("128.237.129.30", "User2");
+		IPMap.put("128.237.130.31", "User3");
+		IPMap.put("128.237.130.32", "User4");
+		IPMap.put("128.237.130.33", "User5");
 
-  static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+	}
 
-    @Override
-    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-      HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
-      responseObserver.onNext(reply);
-      responseObserver.onCompleted();
-    }
-  }
+	/**
+	 * Main launches the server from the command line.
+	 */
+	public static void main(String[] args) throws IOException, InterruptedException {
+		final HelloWorldServer server = new HelloWorldServer();
+		server.start();
+		server.initalizeIPList();
+		server.blockUntilShutdown();
+	}
+
+	static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+		@Override
+		public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+			System.out.println("Recieve message: " + req.getName());
+
+			HelloWorldClient[] clients = new HelloWorldClient[IPMap.size()];
+			int i = 0;
+			for (Map.Entry<String, String> ip : IPMap.entrySet()) {
+				clients[i++] = new HelloWorldClient(ip.getKey(), 50051);
+			}
+
+			String user = req.getName();
+			for (int j = 0; j < clients.length; j++) {
+				try {
+					clients[j].greet(user, IPMap.get(clients[j].getHost()));
+				} catch (Exception e) {
+					System.out.println(IPMap.get(clients[j].getHost()) + " is offline.");
+					IPMap.remove(clients[j].getHost());
+				} finally {
+					System.out.println("You have " + IPMap.size() + " IPs now.");
+					try {
+						clients[j].shutdown();
+					} catch (Exception e) {
+						System.out.println("Error: shutdown " + clients[j].getHost());
+					}
+				}
+			}
+			HelloReply reply = HelloReply.newBuilder().setMessage("Reply from Ruixue Server: " + req.getName()).build();
+			responseObserver.onNext(reply);
+			responseObserver.onCompleted();
+
+		}
+	}
 }
